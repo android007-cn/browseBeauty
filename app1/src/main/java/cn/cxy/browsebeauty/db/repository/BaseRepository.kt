dd83933d4d4538b1527c9f73b66d2e5c390e8c8a
jerry <309032663@qq.com>
2020-11-05 20:08:40 +0800

    update

--- app1/src/main/java/cn/cxy/browsebeauty/db/repository/BaseRepository.kt
+++
@@ -1,7 +0,0 @@
-package cn.cxy.browsebeauty.db.repository
-
-import cn.cxy.browsebeauty.db.AppDatabase
-
-open class BaseRepository {
-    val dbInstance = AppDatabase.getInstance()
-}
--- app1/src/main/java/cn/cxy/browsebeauty/utils/Constants.kt
+++
@@ -1,4 +0,0 @@
-package cn.cxy.browsebeauty.utils
-
-val EXTRA_IMAGE_INFO = "image_info"
-val EXTRA_IMAGE_INFO_POSITION = "image_info_position"
